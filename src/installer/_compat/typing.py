b8e76570ddff495892807e77b8840098c29ab40b
Tzu-ping Chung <uranusjr@gmail.com>
2020-05-05 01:52:06 +0800

    Move __all__ after import statements (#5)

--- src/installer/_compat/typing.py
+++ src/installer/_compat/typing.py
@@ -1,6 +1,6 @@
-__all__ = ["TYPE_CHECKING"]
-
 try:  # pragma: no cover
     from typing import TYPE_CHECKING
 except ImportError:  # pragma: no cover
     TYPE_CHECKING = False
+
+__all__ = ["TYPE_CHECKING"]
