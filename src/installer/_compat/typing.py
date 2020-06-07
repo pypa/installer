557a9d35a470e1e5c04c29f4afe9648b43f01c3c
Pradyun Gedam <pradyunsg@gmail.com>
2020-06-08 01:57:32 +0530

    Combine installer._compat.{builtins, typing}

--- src/installer/_compat/typing.py
+++ src/installer/_compat/typing.py
@@ -1,6 +1,24 @@
+"""Static Typing Support logic
+
+This is shim logic allows this library to use mypy, for static type analysis, while:
+
+- not importing the 'typing' module at runtime.
+- maintaining Python 2 support
+- not completely confusing mypy :)
+"""
+
+__all__ = ["Text", "Binary", "TYPE_CHECKING"]
+
+import sys
+
 try:  # pragma: no cover
     from typing import TYPE_CHECKING
 except ImportError:  # pragma: no cover
     TYPE_CHECKING = False
 
-__all__ = ["TYPE_CHECKING"]
+if sys.version_info >= (3,):  # pragma: no cover
+    Binary = bytes
+    Text = str
+else:  # pragma: no cover
+    Binary = str
+    Text = unicode  # noqa
