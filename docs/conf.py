53d0ef12daed1ec99de5239708b0a51455448b52
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-12-11 10:47:41 +0000

    Improve docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,12 @@
+"""A sphinx documentation configuration file.
+"""
+
 import os
 import pathlib
 import sys
 
-# -- Project information -----------------------------------------------------
+# -- Project information ---------------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 project = "installer"
 
 copyright = "2020, Pradyun Gedam"
@@ -16,9 +20,17 @@ extensions = [
     "myst_parser",
 ]
 
-# -- Options for Autodoc -----------------------------------------------------
+# -- Options for HTML output -----------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_title = project
+
+# -- Options for Autodoc --------------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
 
 autodoc_member_order = "bysource"
+autodoc_preserve_defaults = True
 
 # Automatically extract typehints when not specified and add them to
 # descriptions of the relevant function/methods.
@@ -33,19 +45,16 @@ if "READTHEDOCS" in os.environ:
     __import__("installer")
 
 # -- Options for intersphinx ----------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "pypug": ("https://packaging.python.org", None),
 }
 
-# -- Options for HTML output -------------------------------------------------
-html_theme = "furo"
-html_title = project
+# -- Options for Markdown files --------------------------------------------------------
+# https://myst-parser.readthedocs.io/en/latest/sphinx/reference.html
 
-#
-# -- Options for Markdown files ----------------------------------------------
-#
 myst_enable_extensions = [
     "colon_fence",
     "deflist",
