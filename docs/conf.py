ecb14b400805ee9738883200f1653c07313f7ccf
Pradyun Gedam <pradyunsg@gmail.com>
2022-01-12 06:32:09 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@ project = "installer"
 copyright = "2020, Pradyun Gedam"
 author = "Pradyun Gedam"
 
-
 # -- General configuration -------------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 extensions = [
