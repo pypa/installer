fa3b3287b4e8387f2042a0e7ab5a0393ff09ba58
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-05-19 12:16:31 -0600

    Test on Python 3.14

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ def lint(session):
     session.run("pre-commit", "run", "--all-files", *args)
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "pypy3"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "3.14", "pypy3"])
 def test(session):
     session.install(".")
     session.install("-r", "tests/requirements.txt")
@@ -43,7 +43,7 @@ def test(session):
     )
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "pypy3"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "3.14", "pypy3"])
 def doctest(session):
     session.install(".")
     session.install("-r", "docs/requirements.txt")
